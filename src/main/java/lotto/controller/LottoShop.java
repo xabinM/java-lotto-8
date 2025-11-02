a4c863c39181db3b8856073568b77a20dd4e05eb
xabinM <binine2@naver.com>
2025-11-03 01:12:41 +0900

    feat: 당첨 보너스 번호 입력 기능 구현

--- src/main/java/lotto/controller/LottoShop.java
+++ src/main/java/lotto/controller/LottoShop.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.model.Lotto;
 import lotto.model.LottoAmount;
 import lotto.model.LottoMachine;
+import lotto.model.WinningLotto;
 import lotto.view.LottoView;
 
 import java.util.List;
@@ -18,12 +19,14 @@ public class LottoShop {
 
     public void runLotto() {
         int amount = processPurchaseAmount();
-        processBuyLotto(amount);
+        List<Lotto> lottos = processBuyLotto(amount);
+        WinningLotto winningLotto = processWinningLotto();
     }
 
-    public int processPurchaseAmount() {
+    private int processPurchaseAmount() {
         while (true) {
             try {
+
                 return new LottoAmount(lottoView.requestPurchasePrice()).getAmount();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -31,9 +34,39 @@ public class LottoShop {
         }
     }
 
-    public List<Lotto> processBuyLotto(int amount) {
+    private List<Lotto> processBuyLotto(int amount) {
         List<Lotto> lottos = lottoMachine.buyLottos(amount);
         lottoView.printLottos(lottos);
+
         return lottos;
+    }
+
+    private WinningLotto processWinningLotto() {
+        List<Integer> winningNumbers = drawWinningNumbers();
+        int bonusNumber = drawBonusNumber(winningNumbers);
+
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    private List<Integer> drawWinningNumbers() {
+        while (true) {
+            try {
+
+                return lottoView.requestWinningNumbers();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int drawBonusNumber(List<Integer> winningNumbers) {
+        while (true) {
+            try {
+
+                return lottoView.requestBonusNumber(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
